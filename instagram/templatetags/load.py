b3551ad708f4c0aff50fffccc36ccf8f6a1cb620
Aslbekjon <aslbekxafizov@gmail.com>
2022-04-03 14:32:12 +1200

    who is follow

--- instagram/templatetags/load.py
+++ instagram/templatetags/load.py
@@ -8,9 +8,14 @@ register = template.Library()
 @register.inclusion_tag('who_is_follow.html')
 def show_follow(**df):
     profile = Profile.objects.all()
-    print(df)
-    # k=0
-    # while k!=4:
-    #     follow_status = Following.objects.filter(following=prof, follower=request.user).exists()
+    prof = Profile.objects.get(username=df['profile'])
+    # follow_status = Following.objects.filter(following=prof, follower=profile).exists()
+    # follow_status = Following.objects.exclude(follower=prof)
+
+    # for i in profile:
+    #     follow_status = Following.objects.filter(following=i,follower=prof).exists()
+    #     if not follow_status:
+
+
 
     return {'profile':profile}
