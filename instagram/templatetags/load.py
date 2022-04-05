edcb93fb7db2e14eea749b8fb5ab7852dd643d74
Aslbekjon <aslbekxafizov@gmail.com>
2022-04-05 22:44:00 +1200

    who is follow tugadi

--- instagram/templatetags/load.py
+++ instagram/templatetags/load.py
@@ -10,12 +10,17 @@ def show_follow(**df):
     profile = Profile.objects.all()
     prof = Profile.objects.get(username=df['profile'])
     # follow_status = Following.objects.filter(following=prof, follower=profile).exists()
-    # follow_status = Following.objects.exclude(follower=prof)
+    follow_status = Following.objects.exclude(follower=prof)
+    k=0
+    ls = []
+    for i in profile:
+        follow_status = Following.objects.filter(following=i,follower=prof).exists()
+        if not follow_status and i!=prof:
+            ls.append(i.id)
+        # d = Profile.objects.get(username = i.following)
+        # if not(d.id in ls):
+        #     ls.append(d.id)
 
-    # for i in profile:
-    #     follow_status = Following.objects.filter(following=i,follower=prof).exists()
-    #     if not follow_status:
 
 
-
-    return {'profile':profile}
+    return {'profile':profile, 'ls':ls}
