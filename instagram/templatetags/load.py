36e2e8500a2252ab68372b2c66d98f46b7e90f20
Aslbekjon <aslbekxafizov@gmail.com>
2022-04-26 11:52:18 +0500

    latest posts are outputed

--- instagram/templatetags/load.py
+++ instagram/templatetags/load.py
@@ -1,6 +1,7 @@
 from django import template
 
 from instagramUser.models import Profile, Following
+from instagram.models import Post
 
 
 register = template.Library()
@@ -17,3 +18,12 @@ def show_follow(**df):
             ls.append(i.id)
 
     return {'profile':profile, 'ls':ls}
+
+@register.inclusion_tag('latest.html')
+def show_latest():
+    posts = Post.objects.order_by('-created_at')[:4]
+    return {'posts':posts}
+
+@register.inclusion_tag('comment_index.html')
+def show_comments():
+    return {'son':1}
